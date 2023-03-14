76b4d836c1c79a209d33317fd9abbbb5b5bd4527
Joseph Kehinde Ishola <joseph.arogunyo@yahoo.com>
2023-03-14 22:11:06 +0000

    first commit

---
+++ Thermal Conductivity Testing/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(457, 255)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(30, 255)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Connect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(2, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Lenght (m)"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(2, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Diameter (m)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(74, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(74, 54)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM72"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(5, 105)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(541, 134)
+        Me.ListBox1.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(164, 25)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Voltage (V)"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 89)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 13)
+        Me.Label4.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(257, 22)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(44, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(164, 61)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Name of Material"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(257, 58)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(80, 20)
+        Me.TextBox4.TabIndex = 12
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(558, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(92, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(400, 27)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(558, 290)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Thermal Conductivity"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Thermal Conductivity Testing/Form1.vb
@@ -0,0 +1,191 @@
+﻿Imports System.Threading
+
+Public Class Form1
+    Delegate Sub SetTextCallback(ByVal [text] As String) 'Added to prevent threading errors during receiveing of data
+    Dim port As String
+    Dim RXstring As String
+    Dim Q, Conductiv, temp_Hot, temp_Cold, length, Dia, Area, Prev_Time, sum, counter, power, temp_diff As Single
+
+    Dim splitted() As String
+    
+
+    Const Res As Single = 136.8
+
+    Const pi As Single = 22 / 7
+    Const c As Single = 0.022602
+    Dim time
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+
+
+
+        If (Button2.Text = "Disconnect") And SerialPort1.IsOpen() Then
+            SerialPort1.Close()
+            Button2.Text = "Connect"
+            time = 0
+            Prev_Time = 0
+            Timer1.Enabled = False
+            Label4.Text = "Computed K (W/m K)= " & sum / counter
+
+
+
+        ElseIf (Button2.Text = "Connect") And Not (SerialPort1.IsOpen()) Then
+
+            SerialPort1.Open()
+            Button2.Text = "Disconnect"
+
+            sum = 0
+            counter = 0
+        End If
+
+
+
+
+    End Sub
+
+  
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim myPort As Array
+        Dim pattern As String
+        Dim vid As String
+        Dim pid As String
+        vid = "0023"
+        pid = "002"
+        ' Dim writer As System.IO.StreamWriter
+        'writer = System.IO.File.AppendText("Thermal Conductivity Data.csv")
+        myPort = IO.Ports.SerialPort.GetPortNames
+
+
+
+        ComboBox1.Items.AddRange(myPort)
+
+        pattern = String.Format("^VID_{0}.PID_{1}", vid, pid)
+
+        FileOpen(1, "Thermal Conductivity Data.csv", OpenMode.Output)
+
+
+
+        port = InputBox("Enter device Port", "Com Port", "COM72")
+        SerialPort1.PortName = port
+
+
+
+        header()
+
+        'writer.Write("Program to obtain the thermal conductivity of a material" & vbNewLine)
+        'writer.Write(vbTab & vbTab & Now() & vbNewLine)
+
+
+        'Compute the power
+
+        ' writer.Write("Temperature" & "," & " Thermal Conductivity" & "," & "Time" & vbNewLine)
+        'Label3.Text = "Power =" & power & "W"
+
+
+
+    End Sub
+    Public Sub header()
+
+        Me.ListBox1.Items.Add(vbTab & vbTab & vbTab & "Program to obtain the thermal conductivity of a material" & vbNewLine)
+
+        Print(1, "Program to obtain the thermal conductivity of a material" & vbNewLine)
+
+        Me.ListBox1.Items.Add(vbTab & vbTab & vbTab & vbTab & vbTab & Now() & vbNewLine)
+
+        Print(1, vbTab & vbTab & Now() & vbNewLine)
+
+        Me.ListBox1.Items.Add("___________________________________________________________________________________________________________________")
+        Me.ListBox1.Items.Add("Hot Temp" & vbTab & "Cold Temp" & vbTab & "Temp. Difference" & vbTab & " Thermal Conductivity (W/m K)" & vbTab & "Time")
+        Me.ListBox1.Items.Add("____________________________________________________________________________________________________________________")
+
+        Print(1, "Hot Temp" & "," & "Cold Temp" & "," & "Temp. Difference" & "," & " Thermal Conductivity (W/m K)" & "," & "Time")
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As System.Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ReceivedText(SerialPort1.ReadLine())
+    End Sub
+
+    Private Sub ReceivedText(ByVal [text] As String) 'input from ReadExisting
+        If Me.ListBox1.InvokeRequired Then
+            Dim x As New SetTextCallback(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+        Else
+
+            splitted = Split(text, ";")
+            temp_Hot = Val(splitted(0))
+            temp_Cold = Val(splitted(1))
+            Q = power
+            temp_diff = (temp_Hot - temp_Cold)
+            Conductiv = ((Q * length) / (Area * (temp_diff))) * c
+
+            sum += Conductiv
+
+            counter += 1
+
+            Me.ListBox1.Items.Add(temp_Hot & vbTab & temp_Cold & vbTab & temp_diff & vbTab & vbTab & vbTab & Conductiv & vbTab & vbTab & time & vbNewLine)  'append text
+
+            Print(1, (temp_Hot & "," & temp_Cold & "," & temp_diff & "," & Conductiv & "," & time & vbNewLine))
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        ListBox1.Items.Clear()
+        Label4.Text = ""
+        header()
+        Button2.Enabled = False
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If IsNumeric(TextBox1.Text) And IsNumeric(TextBox2.Text) And IsNumeric(TextBox3.Text) Then
+            Button2.Enabled = True
+            length = TextBox1.Text
+            Dia = TextBox2.Text
+            power = (Val(TextBox3.Text) ^ 2) / Res
+            Area = pi * ((Dia / 2) ^ 2)
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        If IsNumeric(TextBox1.Text) And IsNumeric(TextBox2.Text) And IsNumeric(TextBox3.Text) Then
+            Button2.Enabled = True
+
+            length = TextBox1.Text
+            Dia = TextBox2.Text
+            power = (Val(TextBox3.Text) ^ 2) / Res
+            Area = pi * ((Dia / 2) ^ 2)
+        End If
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.TextChanged
+        If IsNumeric(TextBox1.Text) And IsNumeric(TextBox2.Text) And IsNumeric(TextBox3.Text) Then
+            Button2.Enabled = True
+            length = TextBox1.Text
+            Dia = TextBox2.Text
+            power = (Val(TextBox3.Text) ^ 2) / Res
+            Area = pi * ((Dia / 2) ^ 2)
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        time += 1
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        MsgBox("This program was designed to compute the thermal conductivity of material....", vbInformation + vbOKOnly, "About:: Thermal conductivity")
+    End Sub
+End Class
